71a6fd5ce867715b7713976aa4c4f80818e6c883
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-04-26 10:41:39 +0700

    update folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>{" "}
     </>
   );
 }
