dd36a917525bd5d6b8f617222b3284ca517f7595
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-05-28 00:30:30 +0700

    button custom

--- src/Components/Button/ButtonCustom.jsx
+++ src/Components/Button/ButtonCustom.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const ButtonCustom = ({content}) => {
+const ButtonCustom = ({ content, onClick }) => {
   return (
-    <button>{content}</button>
-  )
-}
+    <button className="py-2 px-5 bg-blue-500 text-white rounded hover:bg-blue-600 duration-500 w-full" onClick={onClick}>
+      {content}
+    </button>
+  );
+};
 
-export default ButtonCustom
+export default ButtonCustom;
--- src/Components/Button/ButtonGoogle.jsx
+++ src/Components/Button/ButtonGoogle.jsx
@@ -3,9 +3,9 @@ import logoGoogle from "../../assets/img/7123025_logo_google_g_icon.svg";
 
 const ButtonGoogle = () => {
   return (
-    <button>
-      <img width="48" height="48" src={logoGoogle} alt="Google Logo" />
-      Continue with Google
+    <button className="flex items-center border border-gray-400 py-2 px-5 rounded hover:bg-gray-300 duration-500 space-x-1 w-full justify-center">
+      <img width="25" height="25" src={logoGoogle} alt="Google Logo" />
+      <span>Continue with Google</span>
     </button>
   );
 };
